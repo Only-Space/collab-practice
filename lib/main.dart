9ff0fabb1048ccb277972a8eac19b96020b863e9
abdul-nut <suryayasaantariksa@gmail.com>
2025-07-19 17:05:52 +0800

    gusanta

--- lib/main.dart
+++ lib/main.dart
@@ -11,7 +11,7 @@ class MyApp extends StatelessWidget {
   @override
   Widget build(BuildContext context) {
     return MaterialApp(
-      title: 'Flutter Demo',
+      title: 'Gusanta Push Inimah',
       theme: ThemeData(
         // This is the theme of your application.
         //
